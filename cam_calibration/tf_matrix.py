0d2fba4a23ac15f6fbfd9246fa2024dabb6de211
Kai0139 <zkrs3914@outlook.com>
2022-10-31 11:50:33 +0000

    test quat manipulations

--- cam_calibration/tf_matrix.py
+++ cam_calibration/tf_matrix.py
@@ -28,8 +28,8 @@ def transformation_matrix(q, t):
     ])
 
     rotm = np.concatenate((R0.as_matrix(), np.array([[0, 0, 0]])), axis=0)
-    rotm = np.concatenate((rotm, np.array([[0, 0, 0, 1]]).T), axis=1)
-
+    rotm = np.concatenate((rotm, np.array([[0, 0, 0, 0]]).T), axis=1)
+    print(rotm)
     tf = rotm + tm
     return tf
 
@@ -42,9 +42,6 @@ q_cam1 = [0.494020, 0.500019, -0.504264, 0.501638]
 t_cam1 = [-0.092345, -0.020508, -0.079230]
 
 transformation_cam1 = transformation_matrix(q_cam1, t_cam1)
-
-q_imu = [0.9999904, -0.0000975849, 0.00362322, 0.002418683]
-t_imu = [-0.0009, 0.0125, 0.0878]
 
 print("transformation matrix of cam0:\n{}".format(transformation_cam0))
 print("inverse tf cam0:\n{}".format(np.linalg.inv(transformation_cam0)))
---
+++ cam_calibration/tf_publisher.py
@@ -0,0 +1,85 @@
+from tkinter import Y
+import rospy
+import tf
+import math
+
+def quat_x(x):
+    qw = math.cos(x/2.0)
+    qx = math.sin(x/2.0)
+    qy = 0.0
+    qz = 0.0
+    return [qw, qx, qy, qz]
+
+def quat_y(y):
+    qw = math.cos(y/2.0)
+    qx = 0.0
+    qy = math.sin(y/2.0)
+    qz = 0.0
+    return [qw, qx, qy, qz]
+
+def quat_z(z):
+    qw = math.cos(z/2.0)
+    qx = 0.0
+    qy = 0.0
+    qz = math.sin(z/2.0)
+    return [qw, qx, qy, qz]
+
+def quat_mult(q, p):
+    qw = q[0]
+    qx = q[1]
+    qy = q[2]
+    qz = q[3]
+
+    pw = p[0]
+    px = p[1]
+    py = p[2]
+    pz = p[3]
+
+    rw = qw*pw - qx*px - qy*py - qz*pz
+    rx = pw*qx + px*qw + py*qz - pz*qy
+    ry = pw*qy - px*qz + py*qw + pz*qx
+    rz = pw*qz + px*qy - py*qx + pz*qw
+    return [rw, rx, ry, rz]
+
+def quat_xyz(x, y, z):
+    quatx = quat_x(x)
+    quaty = quat_y(y)
+    quatz = quat_z(z)
+    quatxyz = quat_mult(quatz, quat_mult(quaty, quatx))
+    return quatxyz
+
+def wxyz_to_xyzw(q):
+    qw = q[0]
+    qx = q[1]
+    qy = q[2]
+    qz = q[3]
+    return [qx, qy, qz, qw]
+
+if __name__ == "__main__":
+    q_cam0_wxyz = (0.500292, 0.490181, -0.508467, 0.500889)
+    q_cam0_xyzw = (0.490181, -0.508467, 0.500889, 0.500292)
+    t_cam0 = (0.067436, -0.022029, -0.078333)
+
+    q_cam1_wxyz = (0.494020, 0.500019, -0.504264, 0.501638)
+    q_cam1_xyzw = (0.500019, -0.504264, 0.501638, 0.494020)
+    t_cam1 = (-0.092345, -0.020508, -0.079230)
+    
+    quatx = quat_x(math.radians(90))
+    q_cam0_wxyz_rot = quat_mult(quatx, q_cam0_wxyz)
+    q_cam1_wxyz_rot = quat_mult(quatx, q_cam1_wxyz)
+
+    q_cam0_xyzw_rot = wxyz_to_xyzw(q_cam0_wxyz_rot)
+    q_cam1_xyzw_rot = wxyz_to_xyzw(q_cam1_wxyz_rot)
+
+    fixed_frame_name = "global"
+
+    rospy.init_node("tf_publisher")
+    lr = rospy.Rate(10)
+    br = tf.TransformBroadcaster()
+
+    while not rospy.is_shutdown():
+        br.sendTransform(t_cam0, tuple(q_cam0_xyzw), rospy.Time.now(), "cam0", fixed_frame_name)
+        br.sendTransform(t_cam1, tuple(q_cam1_xyzw), rospy.Time.now(), "cam1", fixed_frame_name)
+        lr.sleep()
+
+
